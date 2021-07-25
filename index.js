e7b8828b5cb74129f363c849a13193b69659fc55
Berend <berend_kemper@hotmail.com>
2021-07-25 14:30:15 +0200

    v2.2.4 simplified private properties

--- index.js
+++ index.js
@@ -8,242 +8,235 @@ const o_AppendCreat = function loadAppendPlus() {
 const path = require("path");
 const CallbackQueue = require("ca11back-queue");
 const yyyymmdd = require("filestream-logger/lib/yyyymmdd");
-const privFilestreamLoggers = {};
-const getPrivateFilestreamLogger = filestreamLogger => {
-    if (!privFilestreamLoggers[filestreamLogger.dirpath])
-        throw new TypeError(`Can only extend filestreamLoggers, found ${typeof filestreamLogger}`);
-    return privFilestreamLoggers[filestreamLogger.dirpath];
-};
-function makeDir(next) {
-    const req = new FSReqCallback();
-    req.oncomplete = next;
-    mkdir(path.toNamespacedPath(this.dirpath), 0o777, true, req)
-};
-function openFileInitial(next, filepath) {
-    const req = new FSReqCallback();
-    req.oncomplete = openFileInitialAfterOpen;
-    req.logger = this;
-    req.next = next;
-    open(filepath, o_AppendCreat, 0o666, req);
-};
-function openFileInitialAfterOpen(error, fd) {
-    if (error) throw error;
-    this.next(this.logger.fd = fd);
-};
-function queueLogBuffer(next, lineBuffer) {
-    const req = new FSReqCallback();
-    req.oncomplete = logBufferWrite;
-    req.next = next;
-    writeBuffer(this.fd, lineBuffer, 0, lineBuffer.length, null, req);
-};
-function logBufferWrite(error, bytesWritten) {
-    if (error) throw error;
-    this.next();
-};
-class PrivateFilestreamLogger {
-    extending = []
-    extendedFrom = [];
-    queue = new CallbackQueue(this);
-    constructor(filestreamLogger, dirpath, options) {
-        this.public = filestreamLogger;
-        this.dirpath = dirpath;
-        this.filepath = path.join(dirpath, (options?.name || yyyymmdd()) + ".log");
-        if (Array.isArray(options?.extend) && options.extend.length > 0)
-            this.extend(options.extend);
-        if (typeof options?.formatter === "function")
-            this.public.formatter = options.formatter;
-        this.queue.push(makeDir);
-        this.queue.push(openFileInitial, this.filepath);
-    };
-    write(lineBuffer) {
-        this.queue.push(queueLogBuffer, lineBuffer);
-        for (const xLogger of this.extending)
-            xLogger.xWrite(lineBuffer);
-    };
-    xWrite(lineBuffer) {
-        this.queue.push(queueLogBuffer, lineBuffer);
-    };
-    extend(filestreamLoggers) {
-        for (let filestreamLogger of filestreamLoggers) {
-            filestreamLogger = getPrivateFilestreamLogger(filestreamLogger);
-            if (filestreamLogger.extendedFrom.indexOf(this) > -1) throw new Error("Cannot extend a filestreamLogger more than once");
-            filestreamLogger.extendedFrom.push(this);
-            this.extending.push(filestreamLogger);
-        }
-    };
-    destroy(context) {
-        const { removePrivate, callback } = context;
-        removePrivate.call(this.public);
-        this.queue.destroy();
-        this.queue = null;
-        this.public.formatter = null;
-        this.public = null;
-        for (const xLogger of this.extending)
-            xLogger.extendedFrom.splice(xLogger.extendedFrom.indexOf(this), 1);
-        for (const xLogger of this.extendedFrom)
-            xLogger.extending.splice(xLogger.extending.indexOf(this), 1);
-        this.extending = null;
-        this.extendedFrom = null;
-        delete (privFilestreamLoggers[this.dirpath]);
-        callback(this.dirpath);
-    };
-};
 class ExtensibleFunction extends Function {
     constructor(f) {
         return Object.setPrototypeOf(f, new.target.prototype);
     };
 };
-function queueCallback(next, callback) {
-    callback();
-    next();
-};
-function queueSetName(next, context) {
-    context.next = next;
-    context.logger = this
-    const req = new FSReqCallback();
-    req.oncomplete = destroyAfterOpen;
-    req.context = context;
-    context.whichAfterClose = setNameAfterClose;
-    open(context.newFilepath, o_AppendCreat, 0o666, req);
-};
-function queueDestroy(next, context) {
-    context.logger = this
-    context.filepath = this.filepath;
-    const req = new FSReqCallback();
-    req.oncomplete = destroyAfterStat;
-    req.context = context;
-    fstat(req.context.logger.fd, false, req);
-};
-function destroyAfterOpen(error, fd) {
-    if (error) throw error;
-    const req = new FSReqCallback();
-    req.oncomplete = destroyAfterStat;
-    req.context = this.context;
-    req.context.fd = fd;
-    fstat(req.context.logger.fd, false, req);
-};
-function destroyAfterStat(error, stats) {
-    if (error) throw error;
-    const req = new FSReqCallback();
-    req.oncomplete = this.context.whichAfterClose;
-    req.context = this.context;
-    req.context.bytes = (stats[1] & S_IFMT) === S_IFREG ? stats[8] : 0;
-    close(req.context.logger.fd, req);
-};
-function destroyafterClose(error) {
-    if (error) throw error;
-    if (this.context.bytes === 0) {
-        const req = new FSReqCallback();
-        req.oncomplete = destroyAfterUnlink;
-        req.context = this.context;
-        req.context.namespacePath = path.toNamespacedPath(this.context.filepath)
-        return unlink(req.context.namespacePath, req);
-    }
-    this.context.logger.destroy(this.context);
-};
-function destroyAfterUnlink(error) {
-    if (error) throw error;
-    const req = new FSReqCallback();
-    req.oncomplete = destroyAfterReaddir;
-    req.context = this.context;
-    req.context.dirpath = path.toNamespacedPath(req.context.logger.dirpath);
-    readdir(req.context.dirpath, "utf8", false, req);
-};
-function destroyAfterReaddir(error, files) {
-    if (error) throw error;
-    if (files.length === 0) {
-        const req = new FSReqCallback();
-        req.oncomplete = destroyAfterRmdir;
-        req.context = this.context;
-        return rmdir(req.context.dirpath, req);
-    }
-    this.context.logger.destroy(this.context);
-};
-function destroyAfterRmdir(error) {
-    if (error) throw error;
-    this.context.logger.destroy(this.context);
-};
-function setNameAfterClose(error) {
-    if (error) throw error;
-    this.context.logger.fd = this.context.fd;
-    if (this.context.bytes === 0) {
-        const req = new FSReqCallback();
-        req.oncomplete = this.context.next;
-        return unlink(path.toNamespacedPath(this.context.filepath), req);
-    }
-    this.context.next();
-};
+const filestreamLoggers = {};
 /**@callback formattedCallback @param {string} formattedString*/
 /**@callback formatter @param {array} data @param {formattedCallback} callback*/
 /**@callback onDestroyed @param {string} dirpath*/
 class FilestreamLogger extends ExtensibleFunction {
-    #private;
-    #constructMore(type, options) {
-        const dirpath = path.join(options?.dir || "loggers", type);
-        if (privFilestreamLoggers[dirpath])
-            throw new Error(`A logger at dirpath "${dirpath}" already exists`);
-        this.#private = privFilestreamLoggers[dirpath] = new PrivateFilestreamLogger(this, dirpath, options);
-    };
+    #extendedFrom = null;
+    #extendingTo = null;
+    #filepath = null;
+    #dirpath = null;
+    #queue = null;
+    #fd = null;
     /**The fileStreamLogger is a log function and a class instance at the same time. The fileStreamLogger opens files for appending. Logging with fileStreamLogger first formats data into a string and writes a buffer from that string to the file and extended fileStreamLoggers.
-     * @param {string} type @param {{dir:string name:string formatter:formatter extend:array}} options**/
+     * @param {string} type @param {{dir:string name:string formatter:formatter extend:FilestreamLogger[]}} options**/
     constructor(type, options = {}) {
-        super((...data) => this.formatter(data, line => this.#private.write(Buffer.from(line + "\n", "utf8"))));
-        this.#constructMore(type, options);
+        super((...data) => this.formatter(data, line => this.#write(Buffer.from(line + "\n", "utf8"))));
+        this.#dirpath = path.join(options?.dir || "loggers", type);
+        if (filestreamLoggers[this.#dirpath])
+            throw new Error(`A logger at dirpath "${this.#dirpath}" already exists`);
+        filestreamLoggers[this.#dirpath] = this;
+        this.#extendingTo = [];
+        this.#extendedFrom = [];
+        this.#queue = new CallbackQueue(this);
+        this.#filepath = path.join(this.#dirpath, (options?.name || yyyymmdd()) + ".log");
+        if (Array.isArray(options?.extend) && options.extend.length > 0)
+            this.#extend(options.extend);
+        if (typeof options?.formatter === "function")
+            this.formatter = options.formatter;
+        this.#queue.push(this.#queueOpenFile, this.#filepath);
+    };
+    #queueOpenFile(next, filepath) {
+        const req = new FSReqCallback();
+        req.oncomplete = this.#openFileAfterMkdir;
+        req.context = { filepath, next, logger: this };
+        mkdir(path.toNamespacedPath(this.#dirpath), 0o777, true, req);
+    };
+    #openFileAfterMkdir(error) {
+        if (error) throw error;
+        const { context } = this;
+        const req = new FSReqCallback();
+        req.oncomplete = context.logger.#openFileInitialAfterOpen;
+        req.context = context;
+        open(context.filepath, o_AppendCreat, 0o666, req);
+    };
+    #openFileInitialAfterOpen(error, fd) {
+        if (error) throw error;
+        const { context } = this;
+        context.logger.#fd = fd;
+        context.next();
     };
     /**The format method this fileStreamLoggers uses to serialize data.
      * @param {array} data @param {formattedCallback} callback*/
     formatter(data, callback) {
         callback(data.join(" "));
     };
-    /**This method pushes the callback in a queue, the callback is invoked only when all previous queued functions have finished.
-     * @param {function} callback*/
-    onReady(callback) {
-        if (typeof callback !== "function")
-            throw new TypeError("callback must be a function");
-        this.#private.queue.push(queueCallback, callback);
-        return this;
+    #write(lineBuffer) {
+        this.#queue.push(this.#queueLogBuffer, lineBuffer);
+        for (const xLogger of this.#extendingTo)
+            xLogger.#xWrite(lineBuffer);
+    };
+    #xWrite(lineBuffer) {
+        this.#queue.push(this.#queueLogBuffer, lineBuffer);
+    };
+    #queueLogBuffer(next, lineBuffer) {
+        const req = new FSReqCallback();
+        req.oncomplete = this.#logBufferWrite;
+        req.next = next;
+        writeBuffer(this.#fd, lineBuffer, 0, lineBuffer.length, null, req);
+    };
+    #logBufferWrite(error, bytesWritten) {
+        if (error)
+            throw error;
+        this.next();
     };
     /**Extend more filestreamLoggers (even after it's been created).
      * @param {FilestreamLogger} filestreamLogger*/
     extend(filestreamLoggers) {
         Array.isArray(filestreamLoggers)
-            ? this.#private.extend(filestreamLoggers)
-            : this.#private.extend([filestreamLoggers]);
+            ? this.#extend(filestreamLoggers)
+            : this.#extend([filestreamLoggers]);
         return this;
+    };
+    #extend(loggers) {
+        for (let logger of loggers) {
+            if (!filestreamLoggers[logger.#dirpath])
+                throw new TypeError(`Can only extend filestreamLoggers, found ${typeof logger}`);
+            logger = filestreamLoggers[logger.#dirpath];
+            if (logger.#extendedFrom.indexOf(this) > -1)
+                throw new Error("Cannot extend a filestreamLogger more than once");
+            logger.#extendedFrom.push(this);
+            this.#extendingTo.push(logger);
+        }
+    };
+    /**This method pushes the callback in a queue, the callback is invoked only when all previous queued functions have finished.
+     * @param {function} callback*/
+    onReady(callback) {
+        if (typeof callback !== "function")
+            throw new TypeError("callback must be a function");
+        this.#queue.push(this.#queueCallback, callback);
+        return this;
+    };
+    #queueCallback(next, callback) {
+        callback();
+        next();
     };
     /**This method creates a new file to which the fileStreamLoggers logs to and it updates the readable property filepath.
      * @param {string} name*/
     setName(name) {
-        const filepath = this.#private.filepath;
-        const newFilepath = this.#private.filepath = path.join(this.#private.dirpath, name + ".log");
+        const filepath = this.#filepath;
+        const newFilepath = this.#filepath = path.join(this.#dirpath, name + ".log");
         if (newFilepath !== filepath)
-            this.#private.queue.push(queueSetName, {
-                filepath: path.toNamespacedPath(filepath),
-                newFilepath: path.toNamespacedPath(newFilepath),
-                whichAfterClose: setNameAfterClose
+            this.#queue.push(this.#queueSetName, {
+                logger: this,
+                filepathNamespaced: path.toNamespacedPath(filepath),
+                newFilepathNamespaced: path.toNamespacedPath(newFilepath),
             });
         return this;
+    };
+    #queueSetName(next, context) {
+        context.next = next;
+        const req = new FSReqCallback();
+        req.oncomplete = this.#destroyAfterOpen;
+        req.context = context;
+        open(context.newFilepathNamespaced, o_AppendCreat, 0o666, req);
+    };
+    #destroyAfterOpen(error, fd) {
+        if (error) throw error;
+        const req = new FSReqCallback();
+        req.oncomplete = this.context.logger.#destroyAfterStat;
+        req.context = this.context;
+        req.context.fd = fd;
+        fstat(this.context.logger.#fd, false, req);
+    };
+    #setNameAfterClose(error) {
+        if (error) throw error;
+        const { context } = this;
+        context.logger.#fd = context.fd;
+        if (context.bytes === 0) {
+            const req = new FSReqCallback();
+            req.oncomplete = context.next;
+            return unlink(context.filepathNamespaced, req);
+        }
+        context.next();
     };
     /**Closes the file descriptor, destroys the log file at the fileStreamLogger's filepath if it has no content, removes the directory if there were no (log-)files and removes this fileStreamLogger from all fileStreamLoggers that were extended from this fileStreamLogger.
      * @param {onDestroyed} callback*/
     destroy(callback = dirpath => console.log("destroyed", dirpath)) {
-        this.#private.queue.push(queueDestroy, {
-            filepath: path.toNamespacedPath(this.#private.filepath),
-            whichAfterClose: destroyafterClose,
-            removePrivate: this.#removePrivate,
+        this.#queue.push(this.#queueDestroy, {
+            logger: this,
+            filepath: this.#filepath,
             callback,
         });
     };
-    #removePrivate() {
-        this.#private = null;
+    #queueDestroy(next, context) {
+        const req = new FSReqCallback();
+        req.oncomplete = this.#destroyAfterStat;
+        req.context = context;
+        fstat(this.#fd, false, req);
+    };
+    #destroyAfterStat(error, stats) {
+        if (error) throw error;
+        const { context } = this;
+        context.bytes = (stats[1] & S_IFMT) === S_IFREG ? stats[8] : 0;
+        const req = new FSReqCallback();
+        req.oncomplete = context.newFilepathNamespaced
+            ? context.logger.#setNameAfterClose
+            : context.logger.#destroyafterClose;
+        req.context = context;
+        close(context.logger.#fd, req);
+    };
+    #destroyafterClose(error) {
+        if (error) throw error;
+        const { context } = this;
+        if (context.bytes === 0) {
+            const req = new FSReqCallback();
+            req.oncomplete = context.logger.#destroyAfterUnlink;
+            req.context = context;
+            return unlink(path.toNamespacedPath(context.logger.#filepath), req);
+        }
+        context.logger.#destroy(context);
+    };
+    #destroyAfterUnlink(error) {
+        if (error) throw error;
+        const { context } = this;
+        context.dirpathNameSpaced = path.toNamespacedPath(context.logger.#dirpath);
+        const req = new FSReqCallback();
+        req.oncomplete = context.logger.#destroyAfterReaddir;
+        req.context = context;
+        readdir(context.dirpathNameSpaced, "utf8", false, req);
+    };
+    #destroyAfterReaddir(error, files) {
+        if (error) throw error;
+        const { context } = this;
+        if (files.length === 0) {
+            const req = new FSReqCallback();
+            req.oncomplete = context.logger.#destroyAfterRmdir;
+            req.context = context;
+            return rmdir(context.dirpathNameSpaced, req);
+        }
+        context.logger.#destroy(context);
+    };
+    #destroyAfterRmdir(error) {
+        if (error) throw error;
+        this.context.logger.#destroy(this.context);
+    };
+    #destroy(context) {
+        this.#queue.destroy();
+        this.#queue = null;
+        this.formatter = null;
+        for (const xLogger of this.#extendingTo)
+            xLogger.#extendedFrom.splice(xLogger.#extendedFrom.indexOf(this), 1);
+        for (const xLogger of this.#extendedFrom)
+            xLogger.#extendingTo.splice(xLogger.#extendingTo.indexOf(this), 1);
+        this.#extendingTo = null;
+        this.#extendedFrom = null;
+        delete (filestreamLoggers[this.#dirpath]);
+        context.callback(this.#dirpath);
     };
     /**Readable property of the dirpath is used internally to store the fileStreamLogger which allows extending fileStreamLoggers.*/
     get dirpath() {
-        return this.#private.dirpath;
+        return this.#dirpath;
     };
     /**Readable property of the path from the file that is currently being logged to.*/
     get filepath() {
-        return this.#private.filepath;
+        return this.#filepath;
     };
     static destroyAll(callback = () => console.log("closed all fileOperators")) {
         if (typeof callback !== "function") throw new TypeError("callback is not a function");
@@ -253,8 +246,8 @@ class FilestreamLogger extends ExtensibleFunction {
                 process.nextTick(callback);
         };
         let counter = 0;
-        for (const dirpath in privFilestreamLoggers)
-            privFilestreamLoggers[dirpath].public.destroy(awaitCounter, counter++);
+        for (const dirpath in filestreamLoggers)
+            filestreamLoggers[dirpath].destroy(awaitCounter, counter++);
     };
 };
 module.exports = FilestreamLogger;
